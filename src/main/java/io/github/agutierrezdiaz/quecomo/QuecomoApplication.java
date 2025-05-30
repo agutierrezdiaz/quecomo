d0da84fe762c7459d61c35a62e6337a39174066f
Adrián Gutiérrez <agutierrezdiaz90@gmail.com>
2025-05-30 21:49:58 +0200

    Project base

---
+++ src/main/java/io/github/agutierrezdiaz/quecomo/QuecomoApplication.java
@@ -0,0 +1,13 @@
+package io.github.agutierrezdiaz.quecomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuecomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuecomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/agutierrezdiaz/quecomo/QuecomoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.agutierrezdiaz.quecomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuecomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
